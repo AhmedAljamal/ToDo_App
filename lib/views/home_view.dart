0772855b98aa5b42b8c3fef835bf7b1a56b616a4
AhmedAljamal <aljamalahmed036@gmail.com>
2024-01-28 09:09:54 +0200

    custom_home_view_body

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/widgets/custom_app_bar.dart';
 import 'package:todo_app/widgets/custom_floationg_action_button.dart';
+import 'package:todo_app/widgets/custom_home_view_body.dart';
 import 'package:todo_app/widgets/custom_icon_search.dart';
 import 'package:todo_app/widgets/custom_notes_item.dart';
 import 'package:todo_app/widgets/custom_notes_list_view.dart';
@@ -13,23 +14,11 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: CustomFloatingActionButton(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            CustomAppBar(),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(child: NotesListView()),
-          ],
-        ),
-      ),
+      body: CustomHomeViewBody(),
     );
   }
 }
+
+
 
 
---
+++ lib/widgets/custom_home_view_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/custom_app_bar.dart';
+import 'package:todo_app/widgets/custom_notes_list_view.dart';
+
+class CustomHomeViewBody extends StatelessWidget {
+  const CustomHomeViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(child: NotesListView()),
+        ],
+      ),
+    );
+  }
+}
