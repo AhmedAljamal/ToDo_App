cd4c79622172c3d55bd4daf0c875ab6e6663023a
AhmedAljamal <aljamalahmed036@gmail.com>
2024-02-03 10:43:04 +0200

    add multiBlocProvider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_app/Models/note_model.dart';
 import 'package:todo_app/constants.dart';
+import 'package:todo_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:todo_app/views/home_view.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kHiveBox);
   Hive.registerAdapter(NoteModelAdapter());
@@ -16,14 +18,21 @@ class ToDoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: false,
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: false,
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: HomeView(),
       ),
-      home: HomeView(),
     );
   }
 }
